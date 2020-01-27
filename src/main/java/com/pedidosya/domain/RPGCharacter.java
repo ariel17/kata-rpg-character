81ea81c1009db8c79a764a833d597d1d01b8579b
Ariel Gerardo Ríos <ariel.rios@mercadolibre.com>
2020-01-27 11:58:50 -0300

    Adds interview code.

--- src/main/java/com/pedidosya/domain/RPGCharacter.java
+++ src/main/java/com/pedidosya/domain/RPGCharacter.java
@@ -1,4 +1,96 @@
 package com.pedidosya.domain;
 
+import java.util.Objects;
+
 public class RPGCharacter {
+
+    public static final int INITIAL_LEVEL = 1;
+
+    public static final int INITIAL_HEALTH = 1000;
+
+    public static final int HIT_THRESHOLD = 5;
+
+    public static final double HIT_REDUCER = 0.5;
+
+    public static final double HIT_INCREASER = 1.5;
+
+    private int health;
+
+    private int level;
+
+    private boolean alive;
+
+    private String name;
+
+    public RPGCharacter(String name){
+        health = INITIAL_HEALTH;
+        level = INITIAL_LEVEL;
+        alive = true;
+        this.name = name;
+    }
+
+    public RPGCharacter(String name, int level){
+        health = INITIAL_HEALTH;
+        this.level = level;
+        alive = true;
+        this.name = name;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public boolean isAlive() {
+        return alive;
+    }
+
+    public boolean getsHit(RPGCharacter enemy, int damage) {
+        if (enemy.equals(this)) {
+            return false;
+        }
+
+        double realDamage;
+        if (level >= enemy.getLevel() + HIT_THRESHOLD ) {
+            realDamage = damage * HIT_REDUCER;
+        } else if (level <= enemy.getLevel() - HIT_THRESHOLD) {
+            realDamage = damage * HIT_INCREASER;
+        } else {
+            realDamage = damage;
+        }
+
+        health -= realDamage;
+        if (health <= 0) {
+            alive = false;
+            health = 0;
+        }
+        return true;
+    }
+
+    public boolean heal(RPGCharacter ally, int health) {
+        if (ally.equals(this) || !alive) {
+            return false;
+        }
+        this.health += health;
+        if (this.health > 1000) {
+            this.health = 1000;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof RPGCharacter)) return false;
+        RPGCharacter that = (RPGCharacter) o;
+        return Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
 }
--- src/test/java/com/pedidosya/domain/RPGCharacterTest.java
+++ src/test/java/com/pedidosya/domain/RPGCharacterTest.java
@@ -1,11 +1,86 @@
 package com.pedidosya.domain;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class RPGCharacterTest {
+
+    private RPGCharacter character;
+
+    private RPGCharacter other;
+
+    @Before
+    public void setUp() {
+        character = new RPGCharacter("me");
+        other = new RPGCharacter("other");
+    }
+
     @Test
-    public void dummyTest() {
-        Assert.assertTrue(true);
+    public void testNew() {
+        Assert.assertEquals(1, character.getLevel());
+        Assert.assertEquals(1000, character.getHealth());
+        Assert.assertTrue(character.isAlive());
+    }
+
+    @Test
+    public void testHitButAlive() {
+        Assert.assertTrue(character.getsHit(other, 100));
+        Assert.assertEquals(900, character.getHealth());
+        Assert.assertTrue(character.isAlive());
+    }
+
+    @Test
+    public void testHitAndDead() {
+        Assert.assertTrue(character.getsHit(other,2000));
+        Assert.assertEquals(0, character.getHealth());
+        Assert.assertFalse(character.isAlive());
+    }
+
+    @Test
+    public void testHealWhenDead() {
+        Assert.assertTrue(character.getsHit(other, 2000));
+        Assert.assertFalse(character.heal(other, 1000));
+    }
+
+    @Test
+    public void testHealWhenAliveButLessThan1000() {
+        Assert.assertTrue(character.getsHit(other, 400));
+        Assert.assertTrue(character.heal(other,200));
+        Assert.assertEquals(800, character.getHealth());
+    }
+
+    @Test
+    public void testHealWhenAliveButMoreThan1000() {
+        Assert.assertTrue(character.getsHit(other,400));
+        Assert.assertTrue(character.heal(other,500));
+        Assert.assertEquals(1000, character.getHealth());
+    }
+
+    @Test
+    public void testCannotHealMyself() {
+        Assert.assertTrue(character.getsHit(other,400));
+        Assert.assertFalse(character.heal(character,200));
+        Assert.assertEquals(600, character.getHealth());
+    }
+
+    @Test
+    public void testCannotHitMyself() {
+        Assert.assertFalse(character.getsHit(character,400));
+        Assert.assertEquals(1000, character.getHealth());
+    }
+
+    @Test
+    public void testEnemyHitsMoreHarder() {
+        other = new RPGCharacter("powerful", 6);
+        Assert.assertTrue(character.getsHit(other, 50));
+        Assert.assertEquals(925, character.getHealth());
+    }
+
+    @Test
+    public void testEnemyHitsSoftly() {
+        character = new RPGCharacter("powerful", 10);
+        Assert.assertTrue(character.getsHit(other, 50));
+        Assert.assertEquals(975, character.getHealth());
     }
 }
